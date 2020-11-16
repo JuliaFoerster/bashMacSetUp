b00d8e4cd74ee7e65622a9d9735d577b152b4aa4
956258 <julia.foerster@pwc.com>
2020-11-16 18:54:09 +0000

    added installation for iTerm and ohmyzsh

--- setupMac.sh
+++ setupMac.sh
@@ -10,8 +10,8 @@ then
   # A plug in to install plug ins  
   curl -fsSLo $HOME/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-	# vim config
-   curl -fsLO $HOME/.myScriptResources --create-dirs https://raw.githubusercontent.com/JuliaFoerster/bashMacSetUp/master/vim.conf
+  # vim config
+  curl -fsSLo $HOME/.vimrc --create-dirs https://raw.githubusercontent.com/JuliaFoerster/bashMacSetUp/master/vim.conf
  
   echo "Installing Vim plugins..."
   vim +"PlugInstall --sync" +qa
@@ -22,10 +22,23 @@ then
   echo "Installing Sublime ..."
  curl -fsSLo $HOME/.myScriptResources/sublime.dmg --create-dirs https://download.sublimetext.com/Sublime%20Text%20Build%203211.dmg
   hdiutil attach $HOME/.myScriptResources/sublime.dmg
-  cp -R /Volumes/Sublime\ Text/Sublime\ Text.app /Applications/Sublime\ Text.app
+  cp -R /Volumes/Sublime\ Text/Sublime\ Text.app /Appilications/Sublime\ Text.app
   hdiutil unmount /Volumes/Sublime\ Text -force
+fi
+
+if [ "$3" == "iTerm" ]
+then
+  echo "Installing iTerm"
+  curl -fsSLo $HOME/.myScriptResources/iTerm2-3_3_11.zip --create-dirs https://iterm2.com/downloads/stable/iTerm2-3_3_11.zip
+  unzip -o -q  $HOME/.myScriptResources/iTerm2-3_3_11.zip -d /Applications
+fi
+
+if [ "$4" == "ohmyzsh" ]
+then
+  echo "Installing OhMyZsh"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
 
 
-rm -r $HOME/.myScriptResources
+echo "rm -r $HOME/.myScriptResources"
